1dd579bc96c0b8f0bc934fb36525b85856322f8e
snicky <118457634+snicky1915@users.noreply.github.com>
2024-05-31 07:51:21 +0900

    s

--- src/main/java/com/example/app/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.repository;
-
-import com.example.app.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
-}
