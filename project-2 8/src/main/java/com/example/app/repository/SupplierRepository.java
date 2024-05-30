d30f5221fc13c6c5629cf156675da493656c28a4
snicky <118457634+snicky1915@users.noreply.github.com>
2024-05-31 07:17:53 +0900

    ㅇ

--- project-2 8/src/main/java/com/example/app/repository/SupplierRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app.repository;
-
-import com.example.app.model.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
-}
